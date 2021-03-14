624a04bb402a1a885ac7b3cdcabbca0ca6999186
bogalla <brianne.ogallagher@gmail.com>
2021-03-14 12:56:49 -0400

    web client test

--- src/main/java/ca/concordia/engweek/programming/amazeing/mazerunner/MazerunnerAlgorithm.java
+++ src/main/java/ca/concordia/engweek/programming/amazeing/mazerunner/MazerunnerAlgorithm.java
@@ -1,4 +1,6 @@
 package ca.concordia.engweek.programming.amazeing.mazerunner;
+import ca.concordia.engweek.programming.amazeing.mazerunner.repository.WebClientRepository;
+import reactor.core.publisher.Mono;
 
 public class MazerunnerAlgorithm {
     final char wall = '+';
@@ -10,6 +12,8 @@ public class MazerunnerAlgorithm {
     int moveDir = 2; //0: north, 1: west, 2: south, 3:east
     int checkDir = 1; //0: north, 1: west, 2: south, 3:east
     int[] coord = new int[2];
+    boolean keyFound = false;
+    WebClientRepository webClientRepository = new WebClientRepository();
 
     public char goLeft(){
         return wall;
@@ -40,11 +44,17 @@ public class MazerunnerAlgorithm {
                 // hit a wall
                 if(output == wall){
                     output = move(moveDir);
+                    // Check moveDir, found wall
                     if(output == wall){
                         // Change direction of movement --> go right, hug down "rotate 90 degrees" (checkdir + 1) (movedir +1)
+                        moveDir += 1;
+                        moveDir %= 4;
+                        checkDir += 1;
+                        checkDir %= 4;
                     }else if(output == key){
                         // backtrack one step (checkdir +2) AND continue in moveDir
                         // move(checkdir +2)
+                        keyFound = true;
                     }else if(output == closedDoor){
                         // store location of door, keep moving in same direction
                     } else if(output == openedDoor){
@@ -98,16 +108,29 @@ public class MazerunnerAlgorithm {
         // checked moveDir, found wall
         else if(out == wall && !hug){
             // Change direction of movement --> go right, hug down "rotate 90 degrees" (checkdir + 1) (movedir +1)
-
+            moveDir += 1;
+            moveDir %= 4;
+            checkDir += 1;
+            checkDir %= 4;
         }else if(out == key){
             // backtrack one step (checkdir +2) AND continue in moveDir
             // move(checkdir +2)
+            int temp = checkDir + 2;
+            temp %= 4;
+            move(temp);
+            move(moveDir);
         }else if(out == closedDoor){
             // store location of door, keep moving in same direction
         } else if(out == openedDoor){
             // change movedir to the checkdir then change checkdir
         }
         // if we reached here, then we got ' ' successfully and moved
+    }
+
+    public void testWebClient() {
+        webClientRepository.startGame();
+        Character output = webClientRepository.movementRequest('d').block();
+        System.out.println("the output of the webclient is " + output);
     }
 
 }
