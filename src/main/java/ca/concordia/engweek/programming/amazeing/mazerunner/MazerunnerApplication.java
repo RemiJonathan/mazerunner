9d59e33873a4a3f0d9d53691c4814c66e8ec4569
Rémi Jonathan Choquette <rj@Remis-MacBook-Pro.local>
2021-03-14 10:24:20 -0400

    Initial commit

---
+++ src/main/java/ca/concordia/engweek/programming/amazeing/mazerunner/MazerunnerApplication.java
@@ -0,0 +1,13 @@
+package ca.concordia.engweek.programming.amazeing.mazerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazerunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazerunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/concordia/engweek/programming/amazeing/mazerunner/MazerunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.concordia.engweek.programming.amazeing.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazerunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
