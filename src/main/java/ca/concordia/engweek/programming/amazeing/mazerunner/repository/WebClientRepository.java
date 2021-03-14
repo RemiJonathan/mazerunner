9a8a9074bf335d38690476f766831f0242fd1764
Remi Jonathan <contact@remijonathan.com>
2021-03-14 11:13:34 -0400

    Added dummy method

--- src/main/java/ca/concordia/engweek/programming/amazeing/mazerunner/repository/WebClientRepository.java
+++ src/main/java/ca/concordia/engweek/programming/amazeing/mazerunner/repository/WebClientRepository.java
@@ -1,4 +1,14 @@
 package ca.concordia.engweek.programming.amazeing.mazerunner.repository;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class WebClientRepository {
+    WebClient client = WebClient.create("https://51.222.158.215:5001/Player/");
+
+    public char movementRequest(char direction){
+
+
+
+        return 'x';
+    }
 }
